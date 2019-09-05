f5b6802c03bd01e61cac4dd47a698eac65f3d6e7
KoutaroYamada <thringred@gmail.com>
2019-09-05 09:29:14 +0000

    articleモデルのコントローラ・view・ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'articles/new'
+  get 'articles/edit'
+  get 'articles/show'
+  get 'articles/update'
+  get 'articles/destroy'
   root "home#top"
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :articles
+
 end
