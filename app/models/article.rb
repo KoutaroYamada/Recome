f5ed8d718f4ec9680d270422978f24c7a7df17f4
KoutaroYamada <thringred@gmail.com>
2019-09-08 04:19:57 +0000

    articleモデルのバリデーション追加（タグ以外）

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ApplicationRecord
   belongs_to :user
   #タグ機能実装
   acts_as_taggable
+
+  #空欄不許可、同じURL登録不可
+  validates :url, presence: true, uniqueness: true
+  #空欄不許可、文字数は最大400文字まで
+  validates :recommend_comment, presence: true, length: { maximum: 400 }
 end
