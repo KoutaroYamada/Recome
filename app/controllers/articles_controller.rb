c5a2c8a980cb19fa7cb7e19c4f2fd35c4619cb92
KoutaroYamada <thringred@gmail.com>
2019-09-20 10:02:14 +0000

    投稿記事編集画面で更新失敗したときのエラー解決

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,12 +16,12 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.user_id = current_user.id
-    @used_tags = Article.where(user_id: current_user.id).tags_on(:tags).pluck(:name)
     
     if @article.save
       flash[:success] = "登録に成功しました。"
       redirect_to user_path(current_user)
     else
+      @used_tags = Article.where(user_id: current_user.id).tags_on(:tags).pluck(:name)
       flash[:danger] = "登録に失敗しました。"
       render :new
 
@@ -44,6 +44,7 @@ class ArticlesController < ApplicationController
       flash[:success] = "記事の編集が完了しました。"
       redirect_to user_path(current_user)
     else
+      @used_tags = Article.where(user_id: current_user.id).tags_on(:tags).pluck(:name)
       render :edit
     end
 
