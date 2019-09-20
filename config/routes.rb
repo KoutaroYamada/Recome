10f1dd87a2bb9df8e72f68a93086f1efb622e5b7
KoutaroYamada <thringred@gmail.com>
2019-09-20 15:14:09 +0000

    navバーにマイコレクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   resources :users do
     member do
-      get :following, :followers, :favorites,:my_colleciton
+      get :following, :followers, :favorites,:my_collection
       patch :add_favorite_tag
       patch :remove_favorite_tag
     end
