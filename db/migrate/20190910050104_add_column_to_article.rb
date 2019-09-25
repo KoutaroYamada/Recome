ddffaebe7d49d1c035cfe025e705c0460adfeb88
KoutaroYamada <thringred@gmail.com>
2019-09-25 08:39:13 +0000

    テーブルをMySQLに対応

--- db/migrate/20190910050104_add_column_to_article.rb
+++ db/migrate/20190910050104_add_column_to_article.rb
@@ -1,10 +1,10 @@
 class AddColumnToArticle < ActiveRecord::Migration[5.2]
   def change
-    add_column :articles, :thumbnail_image_url, :text, null: false, default: ""
-    add_column :articles, :description, :text, null: false, default: ""
+    add_column :articles, :thumbnail_image_url, :text
+    add_column :articles, :description, :text
     add_column :articles, :title, :string, null: false, default: ""
     add_column :articles, :site_name, :string, null: false, default: ""
-    add_index :articles, :description
-    add_index :articles, :title
+    add_index :articles, :description, length:255
+    add_index :articles, :title, length:255
   end
 end
